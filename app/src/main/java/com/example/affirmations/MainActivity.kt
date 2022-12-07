4f5182a588821c96da9b247ac4df952f3209609e
AmrFiqi <amrfiqi55@gmail.com>
2022-12-07 12:15:17 +0200

    3rd Commit, Added images to the recycle view

--- app/src/main/java/com/example/affirmations/MainActivity.kt
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -3,12 +3,18 @@ package com.example.affirmations
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.affirmations.adapter.ItemAdapter
 import com.example.affirmations.data.Datasource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val myDataset= Datasource().loadAffirmations()
+        val recycleview=findViewById<RecyclerView>(R.id.recycle_view)
+        recycleview.adapter=ItemAdapter(this,myDataset)
+        recycleview.setHasFixedSize(true)
 
     }
 }
