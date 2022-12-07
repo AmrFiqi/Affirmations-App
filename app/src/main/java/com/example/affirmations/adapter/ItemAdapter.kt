69afbcd240fc510b342f47ac6a04cd38e1165d61
AmrFiqi <amrfiqi55@gmail.com>
2022-12-07 07:47:57 +0200

    Second Commit

---
+++ app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
@@ -0,0 +1,30 @@
+package com.example.affirmations.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.affirmations.R
+import com.example.affirmations.model.Affirmation
+
+class ItemAdapter(val context : Context, private val dataset : List<Affirmation>): RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+    // Provide a reference to the views for each data item.
+    // Each data item is just an Affirmation object.
+    class ItemViewHolder(private val view: View) :RecyclerView.ViewHolder(view){
+        val textView: TextView= view.findViewById(R.id.item_title)
+    }
+    //Create new views (invoked by the layout manager)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val adapterLayout=LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
+        return ItemViewHolder(adapterLayout)
+    }
+    //Replace the contents of a view (invoked by the layout manager)
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val item=dataset[position]
+        holder.textView.text = context.resources.getString(item.stringResourceId)
+    }
+    //Return the size of your dataset
+    override fun getItemCount() = dataset.size
+}
